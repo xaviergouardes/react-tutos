ed68d7c4af493f1af83544d58c0bfc0e469901b8
Xavier Gouardes <xavier.gouardes@laposte.net>
2016-12-30 20:21:46 +0100

    menu Drawer

--- material-ui-webpack-example/src/app/AppBarExampleIcon.js
+++ material-ui-webpack-example/src/app/AppBarExampleIcon.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
-import Avatar from 'material-ui/Avatar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 /**
  * A simple example of `AppBar` with an icon on the right.
  * By default, the left icon is a navigation-menu.
  */
-const AppBarExampleIcon = () => (
-  <AppBar
-    title="Title"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-    iconElementLeft={<Avatar>A</Avatar>}
-  />
-);
 
-export default AppBarExampleIcon;
+export default class AppBarExampleIcon extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  render() {
+     return (
+
+      <div>
+
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onTouchTap={this.handleToggle}
+        />
+
+        <Drawer open={this.state.open}>
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+
+      </div>
+    );
+  }
+}
